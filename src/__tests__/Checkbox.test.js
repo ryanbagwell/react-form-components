de71f4b7523cb008fa14203d6b518b2e78b449e4
Ryan Bagwell <ryan@ryanbagwell.com>
2018-04-02 11:42:15 -0400

    fixing test encironment

--- src/__tests__/Checkbox.test.js
+++ src/__tests__/Checkbox.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Checkbox from '../Checkbox';
+import Checkbox from '../CheckBox';
 import {shallow, mount, render, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 
