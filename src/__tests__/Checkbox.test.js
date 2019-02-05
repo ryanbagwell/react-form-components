8997990c8882fbeb659fa02d10e66bf764ce9d60
Ryan Bagwell <ryan@ryanbagwell.com>
2019-02-05 13:41:15 -0500

    upgrading the test suite

--- src/__tests__/Checkbox.test.js
+++ src/__tests__/Checkbox.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Checkbox from '../CheckBox';
 import {shallow, mount, render, configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
--- src/__tests__/SelectBox.test.js
+++ src/__tests__/SelectBox.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SelectBox from 'SelectBox';
 import {shallow, mount, render, configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
